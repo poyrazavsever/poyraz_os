e6e51903c0f74e5e5db2704e01a32bd56559e372
poyrazavsever <poyrazavsever@hotmail.com>
2025-08-22 17:06:43 +0300

    feat: added background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import LiquidChrome from "../components/background"
+
 export const metadata: Metadata = {
   title: "Welcome to Poyraz Os Distribution",
   description: "A modern operating system distribution.",
@@ -13,7 +15,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="h-screen w-screen overflow-hidden">
+        <div className="absolute top-0 left-0 w-screen h-screen">
+          <LiquidChrome
+            baseColor={[0.2, 0.2, 0.4]}
+            speed={0.1}
+            amplitude={0.5}
+            interactive={true}
+          />
+        </div>
         {children}
       </body>
     </html>
